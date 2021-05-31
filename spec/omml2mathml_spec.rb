c5edd172ec4363b988ef750e21ae7f25f3e99efd
Alexande B <abobrikovich@gmail.com>
2021-05-31 16:09:23 +0300

    Update tests to work with different nokogiri versions

--- spec/omml2mathml_spec.rb
+++ spec/omml2mathml_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "nokogiri"
 
 RSpec.describe Omml2Mathml do
   it "has a version number" do
@@ -6,54 +7,71 @@ RSpec.describe Omml2Mathml do
   end
 
   it "processes a document" do
-    html = Omml2Mathml.convert("spec/test.html").sub(/<\?xml[^>]+>/, "").sub(/<!DOCTYPE[^>]+>/, "")
-    expect(html).to be_equivalent_to <<~"OUTPUT"
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:dt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml">
+    input = Omml2Mathml.convert("spec/test.html").sub(/<\?xml[^>]+>/, "").sub(/<!DOCTYPE[^>]+>/, "")
 
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<meta name="ProgId" content="Word.Document"/>
-<meta name="Generator" content="Microsoft Word 15"/>
-<meta name="Originator" content="Microsoft Word 15"/>
-<link rel="File-List" href="Part%201_General.fld/filelist.xml"/>
-<link rel="Edit-Time-Data" href="Part%201_General.fld/editdata.mso"/>
-</head>
+    output = <<~"OUTPUT"
+      <html xmlns:v="urn:schemas-microsoft-com:vml"
+            xmlns:o="urn:schemas-microsoft-com:office:office"
+            xmlns:w="urn:schemas-microsoft-com:office:word"
+            xmlns:dt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882"
+            xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
+            xmlns="http://www.w3.org/TR/REC-html40">
 
-<body lang="EL" link="blue" vlink="purple" style="tab-interval:20.0pt" xml:lang="EL">
+      <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      <meta name="ProgId" content="Word.Document"/>
+      <meta name="Generator" content="Microsoft Word 15"/>
+      <meta name="Originator" content="Microsoft Word 15"/>
+      <link rel="File-List" href="Part%201_General.fld/filelist.xml"/>
+      <link rel="Edit-Time-Data" href="Part%201_General.fld/editdata.mso"/>
+      </head>
 
-<div class="WordSection3">
+      <body lang="EL" link="blue" vlink="purple" style="tab-interval:20.0pt" xml:lang="EL">
 
-  <p class="MsoNormal"><span lang="EN-US" style="mso-fareast-font-family:SimSun;&#10;mso-fareast-theme-font:minor-fareast;mso-ansi-language:EN-US;mso-fareast-language:&#10;ZH-CN" xml:lang="EN-US">This standard applies to the elliptic curves over the finite field </span><mml:math>
-<mml:msub>
-  <mml:mrow>
-    <mml:mrow>
-      <mml:mi>F</mml:mi>
-    </mml:mrow>
-  </mml:mrow>
-  <mml:mrow>
-    <mml:mrow>
+      <div class="WordSection3">
+
+        <p class="MsoNormal"><span lang="EN-US" style="mso-fareast-font-family:SimSun;&#10;mso-fareast-theme-font:minor-fareast;mso-ansi-language:EN-US;mso-fareast-language:&#10;ZH-CN" xml:lang="EN-US">This standard applies to the elliptic curves over the finite field </span><mml:math>
+      <mml:msub>
+        <mml:mrow>
+          <mml:mrow>
+            <mml:mi>F</mml:mi>
+          </mml:mrow>
+        </mml:mrow>
+        <mml:mrow>
+          <mml:mrow>
+            <mml:mi>p</mml:mi>
+          </mml:mrow>
+        </mml:mrow>
+      </mml:msub>
+      </mml:math><span lang="EN-US" style="mso-fareast-font-family:SimSun;mso-fareast-theme-font:minor-fareast;&#10;mso-ansi-language:EN-US;mso-fareast-language:ZH-CN" xml:lang="EN-US"><span style="mso-spacerun:yes">&#xA0;</span>(the prime </span><mml:math>
       <mml:mi>p</mml:mi>
-    </mml:mrow>
-  </mml:mrow>
-</mml:msub>
-</mml:math><span lang="EN-US" style="mso-fareast-font-family:SimSun;mso-fareast-theme-font:minor-fareast;&#10;mso-ansi-language:EN-US;mso-fareast-language:ZH-CN" xml:lang="EN-US"><span style="mso-spacerun:yes">&#xA0;</span>(the prime </span><mml:math>
-<mml:mi>p</mml:mi><mml:msup>
-  <mml:mrow>
-    <mml:mrow>
-      <mml:mn>2</mml:mn>
-    </mml:mrow>
-  </mml:mrow>
-  <mml:mrow>
-    <mml:mrow>
-      <mml:mn>191</mml:mn>
-    </mml:mrow>
-  </mml:mrow>
-</mml:msup>
-</mml:math><span lang="EN-US" style="mso-fareast-font-family:SimSun;mso-fareast-theme-font:minor-fareast;&#10;mso-ansi-language:EN-US;mso-fareast-language:ZH-CN" xml:lang="EN-US">).<p/></span></p>
+      <mml:mo>&gt;</mml:mo>
+      <mml:msup>
+        <mml:mrow>
+          <mml:mrow>
+            <mml:mn>2</mml:mn>
+          </mml:mrow>
+        </mml:mrow>
+        <mml:mrow>
+          <mml:mrow>
+            <mml:mn>191</mml:mn>
+          </mml:mrow>
+        </mml:mrow>
+      </mml:msup>
+      </mml:math><span lang="EN-US" style="mso-fareast-font-family:SimSun;mso-fareast-theme-font:minor-fareast;&#10;mso-ansi-language:EN-US;mso-fareast-language:ZH-CN" xml:lang="EN-US">).<p/></span></p>
 
-</div>
-</body>
-</html>
+      </div>
+      </body>
+      </html>
     OUTPUT
+
+    if Gem::Version.new(Nokogiri::VERSION) < Gem::Version.new("1.11")
+      output.gsub!("\n<mml:mo>&gt;</mml:mo>", "")
+      output.gsub!("http://www.w3.org/TR/REC-html40", "http://www.w3.org/1999/xhtml")
+    else
+      output.gsub!(%r{ xml:lang="[\w-]+"}, "")
+    end
+
+    expect(input).to be_equivalent_to output
   end
 end
